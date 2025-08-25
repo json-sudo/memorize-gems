0bf2e5ff9061846cf3e84bd151579f694be49e78
Jason Omemu <jason.o@onthegosystems.com>
2025-08-25 17:39:30 -0400

    Adding initial boilerplate

---
+++ jest.config.ts
@@ -0,0 +1,11 @@
+import type { Config } from 'jest'
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/test/setupTests.ts'],
+  moduleNameMapper: {
+    '\\.(css|scss)$': '<rootDir>/src/test/styleStub.ts'
+  }
+};
+export default config
